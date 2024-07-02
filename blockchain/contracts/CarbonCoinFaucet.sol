6d3e4dc28b1d972f09b647f8dd6f6a658072bb5d
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2024-07-02 17:33:08 -0300

    feat: create faucet contract

---
+++ blockchain/contracts/CarbonCoinFaucet.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract CarbonCoinFaucet is ERC20 {
+
+    constructor() ERC20("CarbonCoin", "CO2") {admin = msg.sender;}
+
+    address public admin;
+    mapping (address => uint256) public nextMint;
+    uint256 private maximumMintCap = 50;
+    uint256 private mintAmount = 5; 
+    uint256 private mintDelay = 24 * 60 * 60;
+
+    modifier onlyAdmin(){
+        require(msg.sender == admin, "Access forbidden, only admin");
+        _;
+    }
+
+    function mint(address _to, uint256 _predictedGasCost) public onlyAdmin {
+        require(mintAmount > 0, "Minting is not enabled");
+        require(_predictedGasCost < address(this).balance);
+        require(nextMint[_to] < block.timestamp, "Only one mint per address in a row");
+        require(msg.sender.balance > 1000000000000000, "Minimum balance amount of 0.001 ETH");
+        _mint(_to, mintAmount);
+        nextMint[_to] = block.timestamp + mintDelay;
+    }
+
+    function setMaxMintingCap(uint256 _maximumMintCap) public onlyAdmin {
+        maximumMintCap = _maximumMintCap;
+    }
+
+    function setMintAmount(uint256 _mintAmount) public onlyAdmin {
+        mintAmount = _mintAmount;
+    }
+
+    function setMintDelay(uint64 _mintDelay) public onlyAdmin {
+        mintDelay = _mintDelay;
+    }
+}
